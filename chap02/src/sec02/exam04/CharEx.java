a24d75291346118ab9b597bdd953406ab7ae906a
wkdwns0501 <britain0501@naver.com>
2025-11-11 16:53:49 +0900

    문자 (char)

---
+++ chap02/src/sec02/exam04/CharEx.java
@@ -0,0 +1,52 @@
+package sec02.exam04;
+
+public class CharEx {
+	public static void main(String[] args) {
+		// char(2byte)
+		// 하나의 문자를 유니코드로 저장
+		// 유니코드: 세계 각국의 문자들을 2byte(0~65535) 범위에 맵핑한 국제 표준 규약
+		
+		char c1 = 'A' ; // 실제로는 유니코드 값인 65가 메모리에 저장됨
+		char c2 = 65; // 정수 타입이라 정수값도 저장 가능
+//		char cc = -65; // 음수값 저장 불가, 유니코드는 0부터 시작
+		char c3 = '\u0041' ; // 유니코드를 의미 + 16진수 (잘 안씀)
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		char c4 = '가' ;
+		char c5 = 44032;
+		char c6 = '\uAC00' ;
+		
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+		// (참고) 유니코드 값 알아내기
+		char var = '각' ;
+		int unicode = var; // 자동 타입 변환
+		System.out.println(unicode);
+		System.out.println((int) var); // 강제 타입 변환
+		
+		// (참고) 문자를 숫자로 변환
+		// 문자 '0' -> 유니코드 48
+		System.out.println('1' - 0); // 49 - 0
+		System.out.println('1' - '0'); // 49 - 48
+		
+		// 이런 방식은 문자를 숫자로 변환할 때 쓰일 수 있음
+		char digit = '8' ;
+		int numericValue = digit - '0';
+		System.out.println(numericValue);
+		
+		/*
+		 * 중요
+		 * 1) 문자와 문자열은 다르다
+		 * 2) 빈 문자 처리에 대해
+		 */
+//		char c = '' ; // 빈 문자 - 컴파일 에러
+		char c = ' ' ; // 공백 문자(유니코드: 32) - 존재하므로 저장 가능
+//		System.out.println((int) ' ');
+		String str = ""; // 유니코드와 상관없음 - 빈 문자열 저장 가능 
+	}
+}
