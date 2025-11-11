b7d7fe3d1373203f77f1bc8d4046763abfce5ce3
wkdwns0501 <britain0501@naver.com>
2025-11-11 16:54:11 +0900

    문자열 (String)

---
+++ chap02/src/sec02/exam05/StringEx.java
@@ -0,0 +1,12 @@
+package sec02.exam05;
+
+public class StringEx {
+	public static void main(String[] args) {
+		// String: 참조 타입(타입이 대문자로 시작)
+		String name = "장준";
+		String job = "프로그래머";
+		
+		System.out.println(name);
+		System.out.println(job);
+	}
+}
