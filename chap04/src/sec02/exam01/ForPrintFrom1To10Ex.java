23f40df3094b7b8cfead014ed0906b5d3524388e
wkdwns0501 <britain0501@naver.com>
2025-11-13 19:59:34 +0900

    반복문

---
+++ chap04/src/sec02/exam01/ForPrintFrom1To10Ex.java
@@ -0,0 +1,10 @@
+package sec02.exam01;
+
+public class ForPrintFrom1To10Ex {
+	public static void main(String[] args) {
+		// 1부터 10까지 반복해서 출력
+		for (int i = 1; i <= 10; i++){
+			System.out.println(i);
+		}
+	}
+}
