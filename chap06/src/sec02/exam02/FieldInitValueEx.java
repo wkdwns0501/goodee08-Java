6f9765b8994420b657e63252dac90c4d4eec1bc2
wkdwns0501 <britain0501@naver.com>
2025-11-18 16:00:04 +0900

    객체의 필드 접근하기

---
+++ chap06/src/sec02/exam02/FieldInitValueEx.java
@@ -0,0 +1,22 @@
+package sec02.exam02;
+
+public class FieldInitValueEx {
+	public static void main(String[] args) {
+		// 객체 생성
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteField: " + fiv.byteField);
+		System.out.println("shortField: " + fiv.shortField);
+		System.out.println("intField: " + fiv.intField);
+		System.out.println("longField: " + fiv.longField);
+		
+		System.out.println("booleanField: " + fiv.booleanField);
+		System.out.println("charField: " + fiv.charField); // 빈 공백(NULL 문자)
+		
+		System.out.println("floatField: " + fiv.floatField);
+		System.out.println("doubleField: " + fiv.doubleField);
+		
+		System.out.println("arrField: " + fiv.arrField);
+		System.out.println("referenceField: " + fiv.referenceField);
+	}
+}
