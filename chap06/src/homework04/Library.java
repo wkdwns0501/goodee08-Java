4074913341580a9794ad0bf48f3d507a86143e52
wkdwns0501 <britain0501@naver.com>
2025-11-19 22:24:09 +0900

    패키지명 변경

--- chap06/src/homework04/Library.java
+++ chap06/src/homework04/Library.java
@@ -2,54 +2,27 @@ package homework04;
 
 public class Library {
 	
-	// 메세지 출력 메소드
-	String printMessage (String message) {
-		String result = message;
+	// 문자열 합치기 메소드
+	String concatString(String str1, String str2) {
+		String result = str1 + str2;
 		return result;
 	}
 	
-	// 1~100의 합 구하는 메소드
-	int plus1To100 () {
-		int sum = 0;
-		for (int i = 1; i <= 100; i++) {
-			sum += i;
-		}
-		return sum;
-	}
-	
-	// 홀수/짝수 판별 메소드
-	String checkOddEven (int num) {
-		if (num%2 == 0) return "짝수";
-		else return "홀수";
-	}
-	
-	// 영문자 존재 판별 메소드
-	boolean containEng (String str) {
-		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-	        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-	            return true;
-	        } 
-		}
-		return false;
-	}
-	
-	// 문자열에 문자가 몇 개 포함되어 있는지 판별 메소드
-	int containChar (String str, char ch) {
-		int count = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == ch) count++;
-		}
-		return count;
-	}
-	
-	// 구구단 출력 메소드
-	void gugudan (int num) {
-		System.out.println("=== " + num + "단 ===");
-		int result = 0;
-		for (int i = 1; i <= 9; i++) {
-			result = num * i;
-			System.out.println(num + " X " + i + " = " + result);
+	// 정수 곱하기 메소드
+	int integerMultiplication (int num1, int num2) {
+		if (num1 < num2) {
+			System.out.println("계산할 수 없습니다.");
+			return 0;
+		} else {
+			int result = num1 * num2;
+			return result;
 		}
 	}
+	
+	// 동일 문자열 판별 메소드
+	boolean checkSameString (String str1, String str2) {
+		if (str1.equals(str2)) return true;
+		else return false;
+	}
+	
 }
--- chap06/src/homework04/Run.java
+++ chap06/src/homework04/Run.java
@@ -4,22 +4,13 @@ public class Run {
 	public static void main(String[] args) {
 		Library library = new Library();
 		
-		String result1 = library.printMessage("메세지");
+		String result1 = library.concatString("문자열", "합치기");
 		System.out.println(result1);
 		
-		int result2 = library.plus1To100();
-		System.out.println("1부터 100까지의 합: " + result2);
+		int result2 = library.integerMultiplication(3, 3);
+		System.out.println(result2);
 		
-		String result3 = library.checkOddEven(2);
+		boolean result3 = library.checkSameString("문자열", "문자열");
 		System.out.println(result3);
-		
-		String result4 = library.containEng("가나다java") 
-				? "영문자가 있습니다." : "영문자가 없습니다." ;
-		System.out.println(result4);
-		
-		int result5 = library.containChar("abcedfsc", 'c');
-		System.out.println(result5+ "개 포함");
-		
-		library.gugudan(2);
 	}
 }
