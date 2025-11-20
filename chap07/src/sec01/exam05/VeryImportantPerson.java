9e53bd4b8d07791bf997e7cb83bfb3f66064811e
wkdwns0501 <britain0501@naver.com>
2025-11-21 00:23:56 +0900

    final 클래스 - 상속 불가

---
+++ chap07/src/sec01/exam05/VeryImportantPerson.java
@@ -0,0 +1,8 @@
+package sec01.exam05;
+
+// Member를 상속할 수 없음
+// public class VeryImportantPerson extends Member {
+public class VeryImportantPerson {
+	// 상속할 이유가 없고, 오히려 상속하면 오용될 가능성이 있는 클래스는 final로 선언
+	// 대표적인 예: String <- 불변 객체이므로 변형을 방지하기 위해
+}
