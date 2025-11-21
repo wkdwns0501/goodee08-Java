88db1f83fe0831ee11e9d7227575c62573da5f83
wkdwns0501 <britain0501@naver.com>
2025-11-21 17:21:17 +0900

    결제 문제 다형성 사용 풀이

---
+++ chap07/src/sec02/exam03_2/PaymentService.java
@@ -0,0 +1,10 @@
+package sec02.exam03_2;
+
+// 부모 클래스: 공통 결제 서비스 -> 추상화 -> 추상 클래스: 결제 서비스의 미완성된 공통 설계도
+public class PaymentService {
+	// 결제 메소드
+	public void pay(int amount) { // <- 실제로는 실행 안됨
+		System.out.println("결제 금액: " + amount + "원");
+		System.out.println("결제를 처리합니다.");
+	}
+}
