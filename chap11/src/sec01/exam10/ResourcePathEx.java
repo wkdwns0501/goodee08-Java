a6289648f93192aeb5c7719731fbe1afd9f59b4d
wkdwns0501 <britain0501@naver.com>
2025-11-27 17:24:17 +0900

    Class 객체로 파일 경로 얻기

---
+++ chap11/src/sec01/exam10/ResourcePathEx.java
@@ -0,0 +1,15 @@
+package sec01.exam10;
+
+public class ResourcePathEx {
+	public static void main(String[] args) {
+		Class clazz = Car.class;
+		
+		// Class 객체는 해당 클래스 파일의 경로 정보를 가지고 있음
+		// 이를 활용해 다른 리소스 파일의 경로를 얻을 수 있음
+		String photo1Path = clazz.getResource("photo1.jpg").getPath();
+		System.out.println(photo1Path); // 현재 클래스가 있는 위치에서 파일을 찾아 절대 경로로 반환
+		
+		String photo2Path = clazz.getResource("image/photo2.jpg").getPath();
+		System.out.println(photo2Path);
+	}
+}
