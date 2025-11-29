7e9aa5f18323879911f3e032fa58f0bd9ee6cdce
wkdwns0501 <britain0501@naver.com>
2025-11-29 21:33:15 +0900

    Vector

---
+++ chap13/src/sec01/exam03/Board.java
@@ -0,0 +1,25 @@
+package sec01.exam03;
+
+public class Board {
+	private String subject;
+	private String content;
+	private String writer;
+	
+	public Board(String subject, String content, String writer) {
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+}
